f7d2aafc6d6201739f4741e9641fd997d42f0cf4
Maximillian Cartwright <maximilianc@zillowgroup.com>
2020-05-15 15:30:52 -0700

    added directory to new repo

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './index.css';
+import Header from './components/Header';
+import React from 'react';
+import Main from './components/Main';
+import Container from './components/Container';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Container>
+        <Main />
+      </Container>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Container/index.js
@@ -0,0 +1,10 @@
+  
+import React from 'react';
+
+function Container(props) {
+  return (
+    <div className={`container${props.fluid ? '-fluid' : ''}`} {...props} />
+  );
+}
+
+export default Container;
